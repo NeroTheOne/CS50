5ad293392ccaabed3d80c7953dd96db942292588
Janmarc Peconcillo <janmarc.peconcillo@gmail.com>
2018-03-29 03:53:53 +0000

    finished pset2: vigenere

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <ctype.h>
 
+int getAlphaIndex(char c);
+
 int main(int argc, string argv[]) {
     if (argc != 2 || atoi(argv[1]) == 0) {
         printf("try again: ./caesar k[int]\n");
@@ -24,14 +26,12 @@ int main(int argc, string argv[]) {
     for (int i = 0; i < strlen(plaintext); i++) {
         char c = plaintext[i];
         if (isalpha(c)) {
-            int ascii = c;
-            // printf("%i\n", ascii);
             if (isupper(c)) {
-                int alpha_i = ascii - 65 + k;
+                int alpha_i = getAlphaIndex(c) + k;
                 // printf("%i\n", alpha_i);
                 printf("%c", (alpha_i % 26) + 65);
             } else {
-                int alpha_i = ascii - 97 + k;
+                int alpha_i = getAlphaIndex(c) + k;
                 printf("%c", (alpha_i % 26) + 97);
             }
 
@@ -42,5 +42,13 @@ int main(int argc, string argv[]) {
 
     printf("\n");
 
+}
 
+int getAlphaIndex(char c) {
+    int ascii = c;
+    if (isupper(c)) {
+        return ascii - 65;
+    } else if (islower(c)) {
+        return ascii - 97;
+    }
 }
--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -1,15 +1,63 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(int argc, string argv[]) {
-    printf("argc: %i\n", argc);
+int getAlphaIndex(char c);
 
-    while (argc != 2) {
-        printf("./vigenere key[string]");
+int main (int argc, string argv[]) {
+
+    if (argc != 2) {
+        printf("error: no key provided\n");
+        printf("./vigenere [string]\n");
         return 1;
     }
 
-    for (int j = 0; j < strlen(argv[1]); j++) {
-
+    string key = argv[1];
+    // printf("key: %s\n", key);
+    for (int i = 0; i < strlen(key); i++) {
+        if (isdigit(key[i])) {
+            printf("error: key should only consist of alphabetical characters\n");
+            printf("./vigenere [string]\n");
+            return 1;
+        }
     }
+
+    string plaintext = get_string("plaintext: ");
+    while (plaintext[0] == '\0') {
+        plaintext = get_string("plaintext: ");
+    }
+
+    printf("ciphertext: ");
+
+    int k = 0;
+    for (int i = 0; i < strlen(plaintext); i++) {
+        char c = plaintext[i];
+        if (isalpha(c)) {
+            int alpha_i = getAlphaIndex(c) + getAlphaIndex(key[k]);
+            if (isupper(c)) {
+                printf("%c", (alpha_i % 26) + 65);
+            } else {
+                printf("%c", (alpha_i % 26) + 97);
+            }
+            k = k + 1;
+            k = k % strlen(key);
+        } else {
+            printf("%c", plaintext[i]);
+        }
+    }
+
+    printf("\n");
 }
+
+int getAlphaIndex(char c) {
+    int ascii = c;
+    if (isupper(c)) {
+        return ascii - 65;
+    } else if (islower(c)) {
+        return ascii - 97;
+    }
+    return 1;
+}
+
+
